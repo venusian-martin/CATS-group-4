698e3fd867501f5f9c3416cbf4de47ca3bafb512
venusian-martin <62942088+venusian-martin@users.noreply.github.com>
2020-04-19 20:27:53 +0200

    finished script

--- centroid_classifier.py
+++ centroid_classifier.py
@@ -1,42 +1,10 @@
 from sklearn.neighbors import NearestCentroid
 from sklearn.model_selection import LeaveOneOut
+from statistics import mean
 import numpy as np
 import pandas as pd
 import argparse
 
-''' this was an copied from an example in the documentation for me to see the data types etc
-
-from sklearn import datasets, neighbors, linear_model
-
-X_digits, y_digits = datasets.load_digits(return_X_y=True)
-X_digits = X_digits / X_digits.max()
-
-print(type(X_digits), X_digits)
-print(type(y_digits), y_digits)
-'''
-
-''' this part might be better to do in R since the fisher test is much easier to perform
-
-# open the arrayCGH data file
-# append each line as row in numpy array (all_samples)
-# split into train_samples and test_samples
-# shuffle() will randomise the order so we get a different 80/20 split each time
-
-# perform fisher exact tests on each feature (region in each sample)
-# the values of 1 feature for every train_sample is simply that column
-# rank features based on fisher test score
-
-# open the subtype data file (training data part)
-# split into train_y and test_y (make sure the split is the same as the samples!)
-
-in that case it would just be:
-
-# load training arrayCGH data
-# load testing arrayCGH data
-# load training subtype data
-# load testing subtype data
-# load ranked list of features
-'''
 # parse command line arguments for input dataset and output from
 parser = argparse.ArgumentParser(description='run inner loop')
 parser.add_argument('-d')
@@ -58,41 +26,61 @@ y = train["Subgroup"]
 train = train.drop(columns = ["Unnamed: 0", "Subgroup"])
 train = train.reindex(columns = ranked_features)
 train = train.to_numpy()
-print("training data: ", train)
 
 testpath = dir + "/Test" + dir + ".txt"
 test = pd.read_csv(testpath, sep = "\t")
 test_y = test["Subgroup"]
 test = test.drop(columns = ["Unnamed: 0", "Subgroup"])
 test = test.reindex(columns = ranked_features)
-test_X = test.to_numpy()
+test = test.to_numpy()
 
-
-#print(test)
+# initiate classifier
 classifier = NearestCentroid()
+i = 0
+cnt = 0
+increment = 5
+used_features = []
+performances = []
 
-# add top feature to numpy array (X)
-X = train[:,0]
-X = X.reshape(-1, 1)
-classifier.fit(X,y) #fits the NearestCentroid model to the training data
-print("I DID THING")
-print("X shape: ", test_X.shape, "y shape: ", test_y.shape)
-print(type(test_X), type(test_y))
-performance = classifier.score(test_X, test_y) # returns the accuracy of classifier.predict() for this data
-previous_performance = 0 #(placeholder so the loop will work)
-
-while performance > previous_performance:
-    previous_performance = performance
-    X += next_top_feature(s) #need to decide how to do this. add 1 each time? add 5? add
+# feature selection
+for attempt in range(0, train.shape[1] +1, increment):
+    cnt += 1
+    i += increment # how many features to add each time
+    used_features.append(i)
+    X = train[:,0:i]
     loo = LeaveOneOut() # function to compute the indices which split the data so that each sample is used for testing once
-    scores = [] # will store the score of each predicition
-    for train_index, test_index in loo.split(X): # this method gives returns the indices
+    scores = []
+    for train_index, test_index in loo.split(X): # this method gives the indices to use each sample as the test once
         X_train, X_test = X[train_index], X[test_index]
+        #print(X_train)
         y_train, y_test = y[train_index], y[test_index]
         classifier.fit(X_train, y_train)
         score = classifier.score(X_test, y_test)
         scores.append(score)
-        performance = mean(scores)
 
-# now get accuracy for the test data set aside earlier
-accuracy = classifier.score(testDataHere, subtypesForTestData)
+    performance = mean(scores)
+
+    if cnt >= 6:
+        last_5 = performances[-5:]
+        # break if the performance has not improved in the last 5 iterations
+        if min(last_5) >= performance:
+            break
+    performances.append(performance)
+
+best = performances.index(max(performances))
+best_features = used_features[best]
+print(best_features)
+
+best_train = train[:,0:best_features]
+best_test = test[:,0:best_features]
+
+final_clf = classifier.fit(best_train, y)
+final_predict = classifier.predict(best_test)
+final_acc = classifier.score(best_test, test_y)
+
+outpath = dir + "/predictions.csv"
+np.savetxt(outpath, final_predict, fmt='%s', delimiter = ",")
+
+with open('accuracies.txt', 'a') as f:
+    line = ''.join(['Test', dir, ", ", str(final_acc), '\n'])
+    f.write(str(line))
