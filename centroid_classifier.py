907358e5a072d519b09f61fddbca27e85d3ba6f6
venusian-martin <62942088+venusian-martin@users.noreply.github.com>
2020-04-19 13:46:16 +0200

    feature selection and test classifier on test set

--- centroid_classifier.py
+++ centroid_classifier.py
@@ -1,6 +1,8 @@
 from sklearn.neighbors import NearestCentroid
 from sklearn.model_selection import LeaveOneOut
 import numpy as np
+import pandas as pd
+import argparse
 
 ''' this was an copied from an example in the documentation for me to see the data types etc
 
@@ -35,12 +37,48 @@ in that case it would just be:
 # load testing subtype data
 # load ranked list of features
 '''
+# parse command line arguments for input dataset and output from
+parser = argparse.ArgumentParser(description='run inner loop')
+parser.add_argument('-d')
+args = parser.parse_args()
+dir = args.d
 
+# load ranked features
+rankpath = dir + "/ranking.csv"
+ranking = pd.read_csv(rankpath, index_col = 0)
+# remove initial X from feature names
+ranking.index = [f[1:] for f in ranking.index]
+ranked_features = ranking.index
+
+# load training and testing data, drop non-feature columns, and reorder according
+# to ranking.csv
+trainpath = dir + "/Train" + dir + ".txt"
+train = pd.read_csv(trainpath, sep = "\t")
+y = train["Subgroup"]
+train = train.drop(columns = ["Unnamed: 0", "Subgroup"])
+train = train.reindex(columns = ranked_features)
+train = train.to_numpy()
+print("training data: ", train)
+
+testpath = dir + "/Test" + dir + ".txt"
+test = pd.read_csv(testpath, sep = "\t")
+test_y = test["Subgroup"]
+test = test.drop(columns = ["Unnamed: 0", "Subgroup"])
+test = test.reindex(columns = ranked_features)
+test_X = test.to_numpy()
+
+
+#print(test)
 classifier = NearestCentroid()
-# add top feature to input array (X)
-# X =
+
+# add top feature to numpy array (X)
+X = train[:,0]
+X = X.reshape(-1, 1)
 classifier.fit(X,y) #fits the NearestCentroid model to the training data
-performance = classifier.score(test_X, y) # returns the accuracy of classifier.predict() for this data
+print("I DID THING")
+print("X shape: ", test_X.shape, "y shape: ", test_y.shape)
+print(type(test_X), type(test_y))
+performance = classifier.score(test_X, test_y) # returns the accuracy of classifier.predict() for this data
 previous_performance = 0 #(placeholder so the loop will work)
 
 while performance > previous_performance:
@@ -57,4 +95,4 @@ while performance > previous_performance:
         performance = mean(scores)
 
 # now get accuracy for the test data set aside earlier
-classifier.score(testDataHere, subtypesForTestData)
+accuracy = classifier.score(testDataHere, subtypesForTestData)
