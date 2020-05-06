8540bbb2624071c690c72797ae97159db1ee3931
venusian-martin <62942088+venusian-martin@users.noreply.github.com>
2020-05-06 21:22:01 +0200

    improved version of nearest centroid
    requires the output of range_fisher_ranking.R

--- true_kfold.py
+++ true_kfold.py
@@ -12,11 +12,10 @@ df = pd.read_csv("transposed_dataset.txt", delimiter = "\t", index_col = 0)
 # import reordering of rows
 order_iter = pd.read_csv("shuffled_rows_df.txt", delimiter = "\t", index_col = 0)
 
-all_mean_accuracies = []
-fold_accuracies = []
-fold_features = []
+triplet_accuracies = []
+triplet_features = []
 # repeat 50 times:
-for it in range(0, 4):
+for it in range(0, 50):
     # reorder rows
     rows = order_iter['iteration ' + str(it)]
     dataset = df.reindex(index = rows)
@@ -27,24 +26,26 @@ for it in range(0, 4):
     test_indices_2 = dataset.index[33:66]
     test_indices_3 = dataset.index[66:100]
 
-    # slices of the dataframe to get test sets
+    # these indeices are kept get test sets
     test_1 = dataset.iloc[0:33]
     test_2 = dataset.iloc[33:66]
     test_3 = dataset.iloc[66:100]
+
     # import the rankings belonging to these splits in the dataset (range_fisher_ranking.R)
     ranking_1 = pd.read_csv(str(it+1)+"_ranking_1.csv", index_col = 0)
     ranking_2 = pd.read_csv(str(it+1)+"_ranking_2.csv", index_col = 0)
     ranking_3 = pd.read_csv(str(it+1)+"_ranking_3.csv", index_col = 0)
 
-    # repeat for every fold (3)
+    # repeat for every fold
     training = [test_indices_1, test_indices_2, test_indices_3]
     testing = [test_1, test_2, test_3]
     ranks = [ranking_1, ranking_2, ranking_3]
 
-
+    # to store results of each fold
+    accuracies = []
+    features = []
     for tr, te, r in zip(training, testing, ranks):
-
-        # remove initial X from feature names (not found in pandas dataset)
+        # remove initial X from feature names (from loading data into Rstudio)
         ranking = r
         ranking.index = [f[1:] for f in ranking.index]
         ranked_features = ranking.index
@@ -60,9 +61,7 @@ for it in range(0, 4):
 
         classifier = NearestCentroid()
 
-        # feature selection
-        features_fold = []
-        performances_fold = []
+        ### feature selection ###
 
         # loop initialisation
         i = 0
@@ -71,9 +70,10 @@ for it in range(0, 4):
         used_features = []
         performances = []
 
-        # go through adding features (increment) at a time
+        # go through adding features $increment at a time
         for attempt in range(0, train.shape[1]+1, increment):
             cnt += 1
+
             i += increment
             used_features.append(i)
             loo = LeaveOneOut()
@@ -87,7 +87,6 @@ for it in range(0, 4):
                 scores.append(loo_score)
 
             performance = mean(scores)
-
             # break when the performance has not improved in the last 5 iterations
             if cnt >= 6:
                 last_5 = performances[-5:]
@@ -97,15 +96,16 @@ for it in range(0, 4):
             performances.append(performance)
 
         # select the number of features with the highest performance
-        print(performances)
         np.array(performances)
         #index of highest performing fit
         max_performance = np.where(performances == np.amax(performances))[0][0]
-        print(type(max_performance))
-        print(max_performance)
+
+        # index 0 means 5 features, index 1 means 10 features, and so on
         best_number_of_features = increment * (max_performance+1)
+        # retrieve the features used
+        best_features = ranked_features[0:best_number_of_features]
 
-
+        # train the model using the best features
         best_train = train[:,0:best_number_of_features]
         final_clf = classifier.fit(best_train, y)
 
@@ -115,14 +115,37 @@ for it in range(0, 4):
         test = test.to_numpy()
 
         best_test = test[:,0:best_number_of_features]
-        # final prediction
-        final_predict = classifier.predict(best_test)
+        # accuracy using best features
         final_acc = classifier.score(best_test, test_y)
 
-        fold_accuracies.append(final_acc)
-        fold_features.append(ranked_features[0:best_number_of_features])
+        accuracies.append(final_acc)
+        features.append(best_features)
 
-print(fold_accuracies)
-print(fold_features)
+    triplet_accuracies.append(accuracies)
+    triplet_features.append(features)
 
-# save this accuracy to file
+# find highest performance and its index
+max = np.amax(triplet_accuracies)
+
+max_index = np.where(triplet_accuracies == max)
+row_index = max_index[0][0]
+col_index = max_index[1][0]
+
+# get features for this iteration
+max_features = triplet_features[row_index][col_index]
+
+# save features to file
+with open("output/features.txt", 'w') as ff:
+    for f in max_features:
+        ff.write(f)
+        ff.write("\n")
+
+# save all 150 accuracies to a file
+accuracies = []
+for trip in triplet_accuracies:
+    accuracies += trip
+
+with open("output/accuracies.txt", 'w') as af:
+    for acc in accuracies:
+        af.write(str(acc))
+        af.write("\n")
